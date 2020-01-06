306739de2b05791b080d1571729d206daa812cfd
Oluwatobi Samue Omisakin <omisakin.oluwatobi@gmail.com>
2020-01-06 20:23:26 +0200

    Add bottom navigation with icons

--- App.js
+++ App.js
@@ -1,18 +1,28 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import Intro from "./components/Intro";
+import History from "./components/History";
+
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
-import History from "./components/History";
+import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs';
+import { IconButton, Colors } from 'react-native-paper';
 
 const instructions = Platform.select({
     ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
     android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
 });
 
-const AppNavigator = createStackNavigator({
-      Intro: Intro,
-      History: History,
+const Button = <IconButton
+    icon="home"
+    color={Colors.white}
+    size={20}
+    onPress={() => console.log('Pressed')}
+/>;
+
+const MainNavigator = createStackNavigator({
+        Intro: Intro,
+        History: History,
     },
     {
         initialRouteName: 'Intro',
@@ -20,22 +30,58 @@ const AppNavigator = createStackNavigator({
             headerStyle: {
                 backgroundColor: '#f4511e',
             },
-            headerTintColor: '#fff',
+            // headerTintColor: '#fff',
             headerTitleStyle: {
                 fontWeight: 'bold',
             },
         },
-    });
+    }
+);
+
+
+const AppNavigator = createMaterialBottomTabNavigator
+(
+    {
+        Intro: {
+            screen: Intro,
+            navigationOptions: {
+                title: "Home",
+                tabBarIcon: <IconButton icon="home" color={Colors.white} size={18} />
+            }
+        },
+        History: {
+            screen: History,
+            navigationOptions: {
+                title: "Home",
+                tabBarIcon: <IconButton icon="history" color={Colors.white} size={18}/>,
+            }
+        },
+    },
+    {
+        initialRouteName: 'Intro',
+        activeColor: '#f0edf6',
+        inactiveColor: '#3e2465',
+        barStyle: {
+            // backgroundColor: '#694fad',
+            backgroundColor: '#3486eb',
+
+            fontWeight: "bold",
+        },
+        // labeled: false,
+    }
+);
 
 const AppContainer = createAppContainer(AppNavigator);
 
-export default class App extends Component {
+class App extends Component {
     render() {
         return (
             <AppContainer/>
         );
     }
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
     container: {
